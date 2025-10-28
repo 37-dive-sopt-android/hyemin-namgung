afc90845fc0ab0b8328c0cc204fbd2110da841e1
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2025-10-29 00:25:21 +0900

    #4 feat: 텍스트 필드 컴포넌트 구성

--- app/src/main/java/com/sopt/dive/ui/components/CommonTextField.kt
+++ app/src/main/java/com/sopt/dive/ui/components/CommonTextField.kt
@@ -8,35 +8,40 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun CommonTextField(
+    value: String,
     placeMessage: String,
     keyboardOptions: KeyboardOptions,
-    visualTransformation  : VisualTransformation? = null
+    visualTransformation: VisualTransformation? = null,
+    errorMessage: String? = null,
+    onValueChange: () -> Unit
 ) {
 
-    var text by remember { mutableStateOf("") }
 
 
     TextField(
-        value = text,
-        onValueChange = { text = it },
+        value = value,
+        onValueChange = {onValueChange},
         modifier = Modifier
             .fillMaxWidth()
-            .background(
-                color = Color.Transparent
-            ),
+            .background(color = Color.Transparent),
         colors = TextFieldDefaults.colors(
             focusedContainerColor = Color.White, unfocusedContainerColor = Color.Transparent
         ),
+        isError = errorMessage != null,
+        supportingText = {
+            if (errorMessage != null) {
+                Text(
+                    text = errorMessage, fontSize = 12.sp
+                )
+            }
+        },
         placeholder = { Text(placeMessage) },
         singleLine = true,
         keyboardOptions = keyboardOptions,
@@ -44,3 +49,4 @@ fun CommonTextField(
 
     )
 }
+
