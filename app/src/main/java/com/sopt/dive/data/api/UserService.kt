20277b27026d81a6c2a4fdee17601ea765300b99
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2025-11-14 22:41:41 +0900

    #9 fix: api 주소 수정

--- app/src/main/java/com/sopt/dive/data/api/UserService.kt
+++ app/src/main/java/com/sopt/dive/data/api/UserService.kt
@@ -1,7 +1,7 @@
 package com.sopt.dive.data.api
 
 import com.sopt.dive.data.dto.RequestSignupDto
-import com.sopt.dive.data.dto.ResponseUserDto
+import com.sopt.dive.data.dto.ResponseUserBodyDto
 import retrofit2.Call // 요 레트로핏 맞겠지 ?
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -9,13 +9,13 @@ import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface UserService {
-    @POST("/api/v1/users")
+    @POST("api/v1/users")
     fun signup(
         @Body request: RequestSignupDto
-    ): Call<ResponseUserDto>
+    ): Call<ResponseUserBodyDto>
 
-    @GET("/api/v1/users/{id}")
+    @GET("api/v1/users/{id}")
     fun fetchUserInfo(
         @Path("id") id: Int,
-    ): Call<ResponseUserDto>
+    ): Call<ResponseUserBodyDto>
 }
