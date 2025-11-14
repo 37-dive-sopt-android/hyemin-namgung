b5ef5ff139e1f2225f473b522350da92df6814c9
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2025-11-14 22:40:42 +0900

    #9 build: BASE_URL 변수 수정

--- app/src/main/java/com/sopt/dive/data/api/AuthService.kt
+++ app/src/main/java/com/sopt/dive/data/api/AuthService.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthService {
-    @POST("/api/v1/auth/login")
+    @POST("api/v1/auth/login")
     fun login(
         @Body request: RequestLoginDto
     ): Call<ResponseLoginDto>
