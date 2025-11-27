f8dc07b3967af14e778ceaa59e3315f425be1cdd
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2025-11-28 03:37:14 +0900

    #10 #11 refactor: 코드 리팩토링

--- app/src/main/java/com/sopt/dive/model/UserProfile.kt
+++ app/src/main/java/com/sopt/dive/model/UserProfile.kt
@@ -4,5 +4,4 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class HomeProfileInfo(val userName: String, val title: String, val content: String) :
-    Parcelable
+data class HomeProfileInfo(val userName: String, val title: String, val content: String) : Parcelable
